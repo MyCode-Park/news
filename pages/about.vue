a88e51836a643167181ab9f87cbd3b0112cc1901
vmuralimohan <vignesh.muralimohan@perficient.com>
2022-02-05 01:28:46 +0530

    News App Initial push

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <v-container>
+            <h2>About Us</h2>
+
+            <v-layout>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis laborum nulla temporibus maiores laboriosam perferendis ratione ad, rem ipsa exercitationem ipsam sint libero a consectetur suscipit nostrum pariatur dignissimos cumque.
+            </v-layout>
+        </v-container>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card id="app">
+        <v-card-title class="headline"> Title </v-card-title>
+        <v-card-text>
+          <p>Paragraph</p>
+          <p>
+            For more information on Vuetify, check out the
+            <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation </a
+            >.
+          </p>
+          <p>
+            Thank you for developing with Vuetify and I look forward to bringing
+            more exciting features in the future.
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3" />
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt Documentation
+          </a>
+          <br />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: wheat  !important;;
+  margin-top: 60px;
+}
